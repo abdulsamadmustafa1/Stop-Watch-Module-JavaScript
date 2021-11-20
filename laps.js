832e6c94304b5b3edbdf4385024da4b342a59223
Abdul Samad <81043493+abdulsamadmustafa1@users.noreply.github.com>
2021-11-20 23:54:40 +0100

    Add files via upload

---
+++ laps.js
@@ -0,0 +1,85 @@
+window.onload = function(){
+    const minutes = document.getElementById("minutes")
+    const tens = document.getElementById("tens")
+    const seconds = document.getElementById("seconds")
+
+    const startBtn = document.getElementById("start-btn");
+    const stopBtn = document.getElementById("stop-btn");
+    const resetBtn = document.getElementById("reset-btn");
+
+    let minutesSet = 00;
+    let secondsSet = 00;
+    let tensSet = 00;
+    let interval;
+
+    //Start Button
+    startBtn.onclick = function(){
+        clearInterval(interval);
+        interval = setInterval(startTimer, 10);
+        dimEffect();
+
+    }
+    //Stop Button
+    stopBtn.onclick = function(){
+        clearInterval(interval);
+
+        const lap = document.getElementById("laps");
+        const li = document.createElement("li");
+        li.innerHTML = `lap:<span> ${minutes.innerHTML}:${seconds.innerHTML}.${tens.innerHTML}</span>`;
+
+        lap.appendChild(li);
+        dimEffect();
+    };
+
+    //Reset Button
+    resetBtn.onclick = function(){
+        clearInterval(interval);
+        minutesSet = "00";
+        secondsSet = "00";
+        tensSet = "00";
+        minutes.innerHTML = minutesSet;
+        seconds.innerHTML = secondsSet;
+        tens.innerHTML = tensSet;
+
+        document.getElementById("laps").innerHTML = ""
+
+    }
+
+    // Create start Timer Function
+    function startTimer(){
+        tensSet++;
+        if(tensSet<9){
+            tens.innerHTML = "0"+tensSet;
+        }
+        if(tensSet>9){
+            tens.innerHTML = tensSet;
+        }
+
+        if(tensSet>99){
+            secondsSet++;
+            seconds.innerHTML = "0"+secondsSet;
+            tensSet = 0;
+            tens.innerHTML = "0"+0;
+        }
+
+        if(secondsSet>9){
+            seconds.innerHTML = "0"+secondsSet;
+        }
+
+        if(secondsSet==60){
+            minutesSet++;
+            minutes.innerHTML = "0"+minutesSet;
+            secondsSet = 0;
+        }
+        if(minutes>9){
+            minutes.innerHTML = minutesSet;
+        }
+        console.log(tens++);
+    }
+
+    //Diming Efect function
+    function dimEffect(){
+        const lapTime = document.querySelector(".lap-time");
+        lapTime.classList.toggle("diming-effect");
+        }
+};
